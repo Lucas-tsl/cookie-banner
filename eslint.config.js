e9c40896743b6d2e7d1c9d4f9527e49a645ea65a
Lucas Troteseil <lucas.troteseil@groupe-novi.com>
2026-07-02 11:26:57 +0200

    ci: mise en place d'une pipeline GitHub Actions (PHPCS, ESLint, Stylelint)
    Closes #1

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["assets/js/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "script",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        gtag: "readonly",
+      },
+    },
+  },
+];
